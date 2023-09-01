9af943d88bbf9bea5cc794bff01a0020fbc2ab6d
midoks <midoks@163.com>
2023-09-01 16:08:57 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gdrive
 备份你的数据到谷歌云网盘
+
+
+# 快捷安装
+```
+cd /www/server/mdserver-web/plugins && rm -rf gdrive && git clone https://github.com/mw-plugin/gdrive && cd gdrive && rm -rf .git && cd /www/server/mdserver-web/plugins/gdrive && bash install.sh install
+```
--- install.sh
+++ install.sh
@@ -24,31 +24,31 @@ Install_App()
 
         which pip
         if [ $? -eq 0 ];then
-          tmp=$(pip list|grep google-api-python-client|awk '{print $2}')
-          if [ "$tmp" != '2.39.0' ];then
-            pip install --upgrade google-api-python-client
-            pip uninstall google-api-python-client -y
-            pip install -I google-api-python-client==2.39.0 -i https://pypi.Python.org/simple
-          fi
-          tmp=$(pip list|grep google-auth-httplib2|awk '{print $2}')
-          if [ "$tmp" != '0.1.0' ];then
-            pip uninstall google-auth-httplib2 -y
-            pip install -I google-auth-httplib2==0.1.0 -i https://pypi.Python.org/simple
-          fi
-          tmp=$(pip list|grep google-auth-oauthlib|awk '{print $2}')
-          if [ "$tmp" != '0.5.0' ];then
-            pip uninstall google-auth-oauthlib -y
-            pip install -I google-auth-oauthlib==0.5.0 -i https://pypi.Python.org/simple
-          fi
-          tmp=$(pip list|grep -E '^httplib2'|awk '{print $2}')
-          if [ "$tmp" != '0.18.1' ];then
-            pip uninstall httplib2 -y
-            pip install -I httplib2==0.18.1 -i https://pypi.Python.org/simple
-          fi
+            tmp=$(pip list|grep google-api-python-client|awk '{print $2}')
+            if [ "$tmp" != '2.39.0' ];then
+                pip install --upgrade google-api-python-client
+                pip uninstall google-api-python-client -y
+                pip install -I google-api-python-client==2.39.0 -i https://pypi.Python.org/simple
+            fi
+            tmp=$(pip list|grep google-auth-httplib2|awk '{print $2}')
+            if [ "$tmp" != '0.1.0' ];then
+                pip uninstall google-auth-httplib2 -y
+                pip install -I google-auth-httplib2==0.1.0 -i https://pypi.Python.org/simple
+            fi
+            tmp=$(pip list|grep google-auth-oauthlib|awk '{print $2}')
+            if [ "$tmp" != '0.5.0' ];then
+                pip uninstall google-auth-oauthlib -y
+                pip install -I google-auth-oauthlib==0.5.0 -i https://pypi.Python.org/simple
+            fi
+            tmp=$(pip list|grep -E '^httplib2'|awk '{print $2}')
+            if [ "$tmp" != '0.18.1' ];then
+                pip uninstall httplib2 -y
+                pip install -I httplib2==0.18.1 -i https://pypi.Python.org/simple
+            fi
         else
-          pip install -I pyOpenSSL
-          pip install -I google-api-python-client==2.39.0 google-auth-httplib2==0.1.0 google-auth-oauthlib==0.5.0 -i https://pypi.Python.org/simple
-          pip install -I httplib2==0.18.1 -i https://pypi.Python.org/simple
+            pip install -I pyOpenSSL
+            pip install -I google-api-python-client==2.39.0 google-auth-httplib2==0.1.0 google-auth-oauthlib==0.5.0 -i https://pypi.Python.org/simple
+            pip install -I httplib2==0.18.1 -i https://pypi.Python.org/simple
         fi
         echo '正在安装脚本文件...' > $install_tmp
 
